8db399fc1aabcddbaa25b8e6826017d1dc89b163
kowalix007 <kowalix007@gmail.com>
2020-01-21 16:51:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,10 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) 
        self.find_by_name(name)
+     else
+       self.create_by_name(name)
   end 
+end 
   def self.all
     @@all
   end
